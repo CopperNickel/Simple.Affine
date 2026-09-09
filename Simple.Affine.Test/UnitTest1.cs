0cdcb48bb47376ad9802d3a9e209470996ff0dbf
Dmitrii Bychenko <Zwerg.CopperNickel@gmail.com>
2026-09-09 22:05:10 +0100

    Add project files.

---
+++ Simple.Affine.Test/UnitTest1.cs
@@ -0,0 +1,8 @@
+﻿namespace Simple.Affine.Test;
+
+public class UnitTest1 {
+  [Fact]
+  public void Test1() {
+
+  }
+}
---
+++ Simple.Affine/TransformationMatrix.cs
@@ -0,0 +1,138 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Simple.Affine;
+
+/// <summary>
+/// Represents a 2D affine transformation matrix.
+/// </summary>
+public readonly struct TransformationMatrix : IEquatable<TransformationMatrix> {
+  #region Properties
+
+  /// <summary>
+  /// Gets the value of the (1, 1) element of the transformation matrix.
+  /// </summary>
+  public double A11 { get; }
+  /// <summary>
+  /// Gets the value of the (1, 2) element of the transformation matrix.
+  /// </summary>
+  public double A12 { get; }
+  /// <summary>
+  /// Gets the value of the (1, 3) element of the transformation matrix.
+  /// </summary>
+  public double A13 { get; }
+  /// <summary>
+  /// Gets the value of the (2, 1) element of the transformation matrix.
+  /// </summary>
+  public double A21 { get; }
+  /// <summary>
+  /// Gets the value of the (2, 2) element of the transformation matrix.
+  /// </summary>
+  public double A22 { get; }
+  /// <summary>
+  /// Gets the value of the (2, 3) element of the transformation matrix.
+  /// </summary>
+  public double A23 { get; }
+
+  /// <summary>
+  /// Determines the determinant of the transformation matrix.
+  /// </summary>
+  public double Determinant => A11 * A22 - A12 * A21;
+
+  #endregion Properties
+
+  #region Create
+
+  /// <summary>
+  /// Gets the identity transformation matrix.
+  /// </summary>
+  public static TransformationMatrix Identity { get; } = new TransformationMatrix(1, 0, 0, 0, 1, 0);
+
+  /// <summary>
+  /// Gets a transformation matrix that represents an error state, with all elements set to NaN.
+  /// </summary>
+  public static TransformationMatrix Error { get; } = new TransformationMatrix(double.NaN, double.NaN, double.NaN, double.NaN, double.NaN, double.NaN);
+
+  /// <summary>
+  /// Creates a new instance of the <see cref="TransformationMatrix"/> struct with the specified values.
+  /// </summary>
+  /// <param name="a11">The value for the (1, 1) element.</param>
+  /// <param name="a12">The value for the (1, 2) element.</param>
+  /// <param name="a13">The value for the (1, 3) element.</param>
+  /// <param name="a21">The value for the (2, 1) element.</param>
+  /// <param name="a22">The value for the (2, 2) element.</param>
+  /// <param name="a23">The value for the (2, 3) element.</param>
+  public TransformationMatrix(double a11, double a12, double a13, double a21, double a22, double a23) {
+    A11 = a11;
+    A12 = a12;
+    A13 = a13;
+    A21 = a21;
+    A22 = a22;
+    A23 = a23;
+  }
+
+  #endregion Create
+
+  #region Public Methods
+
+  /// <summary>
+  /// Inverses the transformation matrix if it is invertible. If the determinant is zero, returns the Error matrix.
+  /// </summary>
+  /// <returns>The inverse transformation matrix, or the Error matrix if the determinant is zero.</returns>
+  public TransformationMatrix Inverse() {
+    double det = Determinant;
+
+    if (det == 0) {
+      return Error;
+    }
+
+    double invDet = 1.0 / det;
+
+    double a11 = A22 * invDet;
+    double a12 = -A12 * invDet;
+    double a21 = -A21 * invDet;
+    double a22 = A11 * invDet;
+
+    double a13 = (A12 * A23 - A22 * A13) * invDet;
+    double a23 = (A21 * A13 - A11 * A23) * invDet;
+
+    return new TransformationMatrix(a11, a12, a13, a21, a22, a23);
+  }
+
+  /// <summary>
+  /// Returns a string representation of the transformation matrix.
+  /// </summary>
+  /// <returns>String representation of the transformation matrix.</returns>
+  public override string ToString() {
+    return $"[{A11}, {A12}, {A13}; {A21}, {A22}, {A23}]";
+  }
+
+  #endregion Public Methods
+
+  #region IEquatable<TransformationMatrix>
+
+  /// <summary>
+  /// Determines whether the specified <see cref="TransformationMatrix"/> is equal to the current <see cref="TransformationMatrix"/>
+  /// </summary>
+  /// <param name="other">The <see cref="TransformationMatrix"/> to compare with the current <see cref="TransformationMatrix"/>.</param>
+  /// <returns>True if the specified <see cref="TransformationMatrix"/> is equal to the current <see cref="TransformationMatrix"/>; otherwise, false.</returns>
+  public bool Equals(TransformationMatrix other) {
+    return A11 == other.A11 && A12 == other.A12 && A13 == other.A13 &&
+           A21 == other.A21 && A22 == other.A22 && A23 == other.A23;
+  }
+
+  /// <summary>
+  /// Determines whether the specified object is equal to the current <see cref="TransformationMatrix"/>.
+  /// </summary>
+  /// <param name="obj">The object to compare with the current <see cref="TransformationMatrix"/>.</param>
+  /// <returns>True if the specified object is equal to the current <see cref="TransformationMatrix"/>; otherwise, false.</returns>
+  public override bool Equals([NotNullWhen(true)] object? obj) =>
+    obj is TransformationMatrix other && Equals(other);
+
+  /// <summary>
+  /// Returns a hash code for the current <see cref="TransformationMatrix"/>.
+  /// </summary>
+  /// <returns>Hash code for the current <see cref="TransformationMatrix"/>.</returns>
+  public override int GetHashCode() => HashCode.Combine(A11, A12, A13, A21, A22, A23);
+
+  #endregion IEquatable<TransformationMatrix>
+}
